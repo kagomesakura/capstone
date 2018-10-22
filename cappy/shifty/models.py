1b0254c3d998748ecbb3e88abb85f937a8bd0f46
kagomesakura <4mygithub>
2018-10-22 15:33:42 -0700

    schedule app wip

--- cappy/shifty/models.py
+++ cappy/shifty/models.py
@@ -1,4 +1,22 @@
 from django.db import models
 
-# Create your models here.
+# this is the information that will be saved in the database
+# saved needs to be input field, date and time
+
+class userName(models.Model):
+    name = models.CharField(max_length=20)
+    day = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.name + " - " + self.day
+
+
+
+# class userTime(models.Model):
+#     time = models.CharField(max_length=20)
+#
+#     def __str__(self):
+#         return self.time
+
 
--- cappy/shifty/views.py
+++ cappy/shifty/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+#from .models import userDay, userName, userTime
 
 # Create your views here.
 def index(request):
      return render(request, 'shifty/index.html', {})
+
+
+
