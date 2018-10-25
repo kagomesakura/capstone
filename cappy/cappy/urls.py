61ac8bcd73b28f471c4fe11d75793334644f226f
kagomesakura <4mygithub>
2018-10-25 16:27:15 -0700

    schedule app wip

--- cappy/cappy/urls.py
+++ cappy/cappy/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('schedule/', include('shifty.urls'), name='index'),
+    path('', include('shifty.urls'), name='index'),
     path('admin/', admin.site.urls),
 ]
